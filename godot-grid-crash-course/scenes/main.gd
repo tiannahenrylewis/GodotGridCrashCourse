b26755abf5ade652f15be3c69071a315d4c3285a
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2024-10-12 20:48:03 -0400

    add TileMapLayer and basic grid operations

--- godot-grid-crash-course/scenes/main.gd
+++ godot-grid-crash-course/scenes/main.gd
@@ -4,6 +4,10 @@ extends Node2D
 
 @onready var cursor: Sprite2D = $Cursor
 @onready var place_building_button: Button = $PlaceBuildingButton
+@onready var highlight_tile_map_layer: TileMapLayer = $HighlightTileMapLayer
+
+var hovered_grid_cell: Vector2 = Vector2(-1, -1)
+var null_cell_value = Vector2(-10,-10)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,11 +15,17 @@ func _ready() -> void:
 	place_building_button.pressed.connect(on_button_pressed)
 	cursor.visible = false
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var grid_position = get_mouse_grid_cell_position()
 	# set the cursor to the mouse position
 	cursor.global_position = grid_position * 64 
+	
+	if cursor.visible && (hovered_grid_cell != null_cell_value || hovered_grid_cell != grid_position):
+		 # reassign the hovered_grid_cell
+		hovered_grid_cell = grid_position
+		update_highlight_tilemap_layer()
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -41,7 +51,24 @@ func place_building_at_mouse_position():
 	add_child(building)
 	var grid_position = get_mouse_grid_cell_position()
 	building.global_position = grid_position * 64
+	
+	# reset the hover state and clear the tilemap to remove highlight cell after placing a building
+	hovered_grid_cell = null_cell_value
+	update_highlight_tilemap_layer()
 
+
+func update_highlight_tilemap_layer():
+	# should by default clear the tilemap everytime it is called
+	highlight_tile_map_layer.clear()
+	
+	if hovered_grid_cell == null_cell_value:
+		pass
+
+	# iterate over all grid cells within a 3-cell radius 
+	for x in range(hovered_grid_cell.x - 3, hovered_grid_cell.x + 4):
+		for y in range(hovered_grid_cell.y - 3, hovered_grid_cell.y + 4):
+			# paint tiles in tilemap
+			highlight_tile_map_layer.set_cell(Vector2(int(x), int(y)), 0, Vector2i.ZERO)
 
 func on_button_pressed():
 	cursor.visible = true
