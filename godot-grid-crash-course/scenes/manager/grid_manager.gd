d0ae8601cfd51eb59c3fa593748ba4a1e754ffee
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2025-05-02 18:22:41 -0400

    Accoutnting for Trees in Grid Manager

--- godot-grid-crash-course/scenes/manager/grid_manager.gd
+++ godot-grid-crash-course/scenes/manager/grid_manager.gd
@@ -5,6 +5,8 @@ extends Node
 @export var highlight_tile_map_layer: TileMapLayer 
 @export var base_terrain_tile_map_layer: TileMapLayer
 
+var _all_tile_map_layers: Array[TileMapLayer] = []
+
 var valid_buildable_tiles = {}
 #OCCUPIED_CELLS &  CONVIENCENE METHODS
 #var my_set = {}
@@ -25,17 +27,19 @@ var valid_buildable_tiles = {}
 func _ready() -> void:
 	var game_events = get_node("/root/GameEvents")
 	game_events.connect("building_placed_signal", _on_building_placed)
+	_all_tile_map_layers = _get_all_tile_map_layers(base_terrain_tile_map_layer)
 
 # is a tile valid in principle
+# perform a depth-first search to check for validity across different tile layers
 func is_tile_position_valid(tile_position: Vector2i) -> bool:
-	var customData = base_terrain_tile_map_layer.get_cell_tile_data(tile_position)
-	
-	#If data is not available then return false
-	if customData == null:
-		return false
-	
-	#If the tile is not marked as `buildable` return false
-	return customData.get_custom_data("buildable")
+	for layer in _all_tile_map_layers:
+		var customData = layer.get_cell_tile_data(tile_position)
+		#If data is not available then return false
+		if customData == null:
+			continue
+		#If the tile is not marked as `buildable` return false
+		return customData.get_custom_data("buildable")
+	return false
 
 
 # is tile in a state where a building can be placed on it, in range of another building
@@ -82,6 +86,25 @@ func get_mouse_grid_cell_position() -> Vector2i:
 
 
 #PRIVATE METHODS
+
+func _get_all_tile_map_layers(root_tile_map_layer: TileMapLayer) -> Array[TileMapLayer]:
+	#print("*** Getting tile map layers for: " + root_tile_map_layer.name)
+	var result: Array[TileMapLayer] = []
+	#reverse the children of tile map layer so that the check is the right order
+	var children = root_tile_map_layer.get_children()
+	children.reverse()
+	# iterate over each each child of tile map layer - returns an array of nodes
+	for child in children:
+		# check if the returned node from get_children() is in fact a TileMapLayer node
+		if child is TileMapLayer:
+			# recursive call to perform the same check on the child and add to the result array
+			result.append_array(_get_all_tile_map_layers(child))
+	#print("*** Adding tile map layer to result: " + root_tile_map_layer.name)
+	result.append(root_tile_map_layer)
+	#print("*** Result: ")
+	for item in result:
+		print("   " + item.name)
+	return result
 
 func _update_valid_buildable_tiles(building_component: BuildingComponent):
 	var root_cell = building_component.get_grid_cell_position()
